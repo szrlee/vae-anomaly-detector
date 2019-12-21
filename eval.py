39395d94cecd20a0a18ea5a4798f00e547215dba
Richard <szrlee@gmail.com>
2019-12-21 20:12:25 +0800

    logsumexp

--- eval.py
+++ eval.py
@@ -9,6 +9,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from configparser import ConfigParser
 from datetime import datetime
+from scipy.special import logsumexp
 
 from vae.vae import VAE
 from utils.data import SpamDataset
@@ -92,6 +93,8 @@ def eval(config, testloader):
     print(mean_confidence_interval(recalls))
     all_log_densities = np.concatenate(all_log_densities, axis=1)
     print(all_log_densities.shape)
+    batch_log_densities = logsumexp(all_log_densities, axis=1)
+    print(batch_log_densities.shape)
     # storage['log_densities'] = self._get_densities(trainloader)
     # storage['params'] = self._get_parameters(trainloader)
     # with open('./results/{}.pkl'.format(self.model_name), 'wb') as _f:
